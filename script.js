a74a51395144b440ca03b979736c37fc1b1d64e1
Ashwiniyadav23 <ashwiniyadav23@navgurukul.org>
2024-10-24 09:49:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-// const panels = document.querySelectorAll(".panel");
+const panels = document.querySelectorAll(".panel");
 
-// panels.forEach((panel, index) => {
-//   panel.addEventListener("click", () => {
-//     removeActiveClasses();
-//     panel.classList.add("active");
-//   });
-// });
+panels.forEach((panel, index) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
 
-// function removeActiveClasses() {
-//   panels.forEach(panel => {
-//     panel.classList.remove("active");
-//   });
-// }
+function removeActiveClasses() {
+  panels.forEach(panel => {
+    panel.classList.remove("active");
+  });
+}
 
-// let currentPanelIndex = 0;
-// document.addEventListener("keydown", (e) => {
-//   if (e.key === "ArrowRight") {
-//     currentPanelIndex = (currentPanelIndex + 1) % panels.length;
-//   } else if (e.key === "ArrowLeft") {
-//     currentPanelIndex = (currentPanelIndex - 1 + panels.length) % panels.length;
-//   }
+let currentPanelIndex = 0;
+document.addEventListener("keydown", (e) => {
+  if (e.key === "ArrowRight") {
+    currentPanelIndex = (currentPanelIndex + 1) % panels.length;
+  } else if (e.key === "ArrowLeft") {
+    currentPanelIndex = (currentPanelIndex - 1 + panels.length) % panels.length;
+  }
 
-//   removeActiveClasses();
-//   panels[currentPanelIndex].classList.add("active");
-// });
+  removeActiveClasses();
+  panels[currentPanelIndex].classList.add("active");
+});
